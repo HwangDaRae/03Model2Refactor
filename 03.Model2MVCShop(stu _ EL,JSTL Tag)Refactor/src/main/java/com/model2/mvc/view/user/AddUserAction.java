28466ae46ac4e0eb2248e90c8809478084a28c61
bitcamp <bitcamp@DESKTOP-26ANI3M>
2022-08-03 00:44:36 +0900

    new/dev : User : 2022.08.02 good night

--- 03.Model2MVCShop(stu _ EL,JSTL Tag)Refactor/src/main/java/com/model2/mvc/view/user/AddUserAction.java
+++ 03.Model2MVCShop(stu _ EL,JSTL Tag)Refactor/src/main/java/com/model2/mvc/view/user/AddUserAction.java
@@ -12,22 +12,21 @@ import com.model2.mvc.service.user.impl.UserServiceImpl;
 public class AddUserAction extends Action {
 
 	@Override
-	public String execute(	HttpServletRequest request,
-												HttpServletResponse response) throws Exception {
-		User userVO=new User();
-		userVO.setUserId(request.getParameter("userId"));
-		userVO.setPassword(request.getParameter("password"));
-		userVO.setUserName(request.getParameter("userName"));
-		userVO.setSsn(request.getParameter("ssn"));
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		User user=new User();
+		user.setUserId(request.getParameter("userId"));
+		user.setPassword(request.getParameter("password"));
+		user.setUserName(request.getParameter("userName"));
+		user.setSsn(request.getParameter("ssn"));
+		user.setAddr(request.getParameter("addr"));
+		user.setPhone(request.getParameter("phone"));
+		user.setEmail(request.getParameter("email"));
 		
-		userVO.setAddr(request.getParameter("addr"));
-		userVO.setPhone(request.getParameter("phone"));
-		userVO.setEmail(request.getParameter("email"));
+		System.out.println("AddUserAction ::"+user);
 		
-		System.out.println(userVO);
-		
-		UserService service=new UserServiceImpl();
-		service.addUser(userVO);
+		UserService userService=new UserServiceImpl();
+		userService.addUser(user);
 		
 		return "redirect:/user/loginView.jsp";
 	}
--- 03.Model2MVCShop(stu _ EL,JSTL Tag)Refactor/src/main/java/com/model2/mvc/view/user/LoginAction.java
+++ 03.Model2MVCShop(stu _ EL,JSTL Tag)Refactor/src/main/java/com/model2/mvc/view/user/LoginAction.java
@@ -13,22 +13,17 @@ import com.model2.mvc.service.user.impl.UserServiceImpl;
 public class LoginAction extends Action{
 
 	@Override
-	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		User userVO=new User();
-		//단일인입점 ActionServlet에서 받은 request에 있는 정보를 VO에 넣는다
-		userVO.setUserId(request.getParameter("userId"));
-		userVO.setPassword(request.getParameter("password"));
+	public String execute(HttpServletRequest request,	HttpServletResponse response) throws Exception {
 		
-		UserService service=new UserServiceImpl();
-		//로그인은 id, pwd만 있다 dbVO는 로그인 회원의 모든정보 있다
-		User dbVO=service.loginUser(userVO);
+		User user=new User();
+		user.setUserId(request.getParameter("userId"));
+		user.setPassword(request.getParameter("password"));
+		
+		UserService userService=new UserServiceImpl();
+		User dbUser=userService.loginUser(user);
 		
 		HttpSession session=request.getSession();
-		session.setAttribute("user", dbVO);
-		
-		//if(dbVO.getRole().equals("admin")) {
-		//	session.setAttribute("admin", "admin");
-		//}
+		session.setAttribute("user", dbUser);
 		
 		return "redirect:/index.jsp";
 	}
